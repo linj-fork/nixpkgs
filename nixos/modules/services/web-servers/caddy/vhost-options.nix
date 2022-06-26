f119a427c5d3bed7fb61bfbef0a3f7b4f67f53d9
Lin Jian <me@linj.tech>
2022-06-26 14:19:01 +0800

    nixos/caddy: don't set default logFormat for vhost
    In this case, the upsteam default (console) is used and access logs
    are managed by systemd-journald.
    
    defaultText is not changed to avoid building the nixos manual.

--- nixos/modules/services/web-servers/caddy/vhost-options.nix
+++ nixos/modules/services/web-servers/caddy/vhost-options.nix
@@ -57,9 +57,8 @@ in
 
     logFormat = mkOption {
       type = types.lines;
-      default = ''
-        output file ${cfg.logDir}/access-${lib.replaceStrings [ "/" " " ] [ "_" "_" ] config.hostName}.log
-      '';
+      default = "";
+      # don't change defaultText to avoid building the manual
       defaultText = ''
         output file ''${config.services.caddy.logDir}/access-''${hostName}.log
       '';
