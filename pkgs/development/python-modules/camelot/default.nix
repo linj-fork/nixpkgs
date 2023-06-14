d29c378ca30637fb8e47f2489a46e149cd7b8dfe
Lin Jian <me@linj.tech>
2023-06-14 15:29:50 +0800

    python310Packages.camelot: remove runtime check for libgs

--- pkgs/development/python-modules/camelot/default.nix
+++ pkgs/development/python-modules/camelot/default.nix
@@ -30,6 +30,10 @@ buildPythonPackage rec {
 
   nativeBuildInputs = [ setuptools ];
 
+  patches = [
+    ./remove-runtime-check-for-libgs.patch
+  ];
+
   propagatedBuildInputs = [
     charset-normalizer
     chardet
